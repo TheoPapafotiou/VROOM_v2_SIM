541a9de53639b1e4026eeca3fdc2c0cc84ad3d99
Evi Nestoropoulou <evi.nestoro@gmail.com>
2022-03-09 20:43:05 +0200

    add minor changes [spacing , libraries]

--- Simulator/src/example/src/intersection.py
+++ Simulator/src/example/src/intersection.py
@@ -1,10 +1,8 @@
-from mimetypes import init
 import numpy as np
 import cv2 
 import time
-from skimage.measure import ransac, LineModelND  
-# get gia frame
-    
+
+
 class Intersection:
 
     def __init__(self, width, height, get_perception):
@@ -15,9 +13,9 @@ class Intersection:
         self.x_points = []
         self.y_points = []
         self.finished = False  
-        self.reached= False
-        self.lane_frame_int = np.zeros((self.height, self.width, 3), dtype = np.uint8) 
-        # self.img = np.zeros((self.height, self.width, 3), dtype = np.uint8)  
+        self.reached = False
+        self.lane_frame_int = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        # self.img = np.zeros((self.height, self.width, 3), dtype=np.uint8)
       
         self.yaw_diff = 0
         self.increase_angle = False
@@ -25,13 +23,13 @@ class Intersection:
         self.yaw_angle = False
         self.angle_step = 1
 
-        #image processing parameters
+        # image processing parameters
         self.ThresholdHigh = 150
         self.ThresholdLow = 350
         self.KSize = (5, 5)
         self.BorderType = 0
 
-        #thresh_callback parameters
+        # thresh_callback parameters
         self.xmax = 0
         self.wmax = 0
         self.ymax = 0
@@ -45,18 +43,18 @@ class Intersection:
 
     def mask(self, image, case):
         if case == "Straight":
-            polygons=np.array([
-                [(int(self.width*0.35),int(self.height/3.5)),(int(self.width/3),int(self.height*0.45)),(int(self.width*0.5),int(self.height*0.45)),(int(self.width*0.5),int(self.height/3.5))]
+            polygons = np.array([
+                [(int(self.width*0.35), int(self.height/3.5)), (int(self.width/3), int(self.height*0.45)), (int(self.width*0.5), int(self.height*0.45)), (int(self.width*0.5), int(self.height/3.5))]
                 ])
 
         elif case == "SmallRight":  
-            polygons=np.array([
-                [(int(self.width/3), int(self.height*0.45)),(self.width,int(self.height*0.45)),(self.width,int(self.height*0.65)),(int(self.width/3),int(self.height*0.65))]
+            polygons = np.array([
+                [(int(self.width/3), int(self.height*0.45)), (self.width, int(self.height*0.45)), (self.width, int(self.height*0.65)), (int(self.width/3), int(self.height*0.65))]
                 ])
 
         elif case == "BigLeft":
-            polygons=np.array([
-                [(int(self.width/4), int(self.height*0.35)),(self.width*0.6,int(self.height*0.35)),(self.width*0.5,int(self.height*0.65)),(int(self.width/4),int(self.height*0.65))]
+            polygons = np.array([
+                [(int(self.width/4), int(self.height*0.35)), (self.width*0.6, int(self.height*0.35)), (self.width*0.5, int(self.height*0.65)), (int(self.width/4), int(self.height*0.65))]
                 ])
         mask = np.zeros_like(image)
         cv2.fillPoly(mask, np.int32([polygons]), 255)
@@ -71,7 +69,7 @@ class Intersection:
         contours = contours[0] if len(contours) == 2 else contours[1]
 
         for c in contours:
-            x,y,w,h = cv2.boundingRect(c)
+            x, y, w, h = cv2.boundingRect(c)
 
             if ((x+w) > self.width*0.7) and (w > 40):
                 self.xmax = x 
@@ -121,10 +119,11 @@ class Intersection:
 
         return (cx, cy), radius, point1_angle, point2_angle
 
-    def draw_ellipse(self, img, center, axes, angle, startAngle, endAngle, color):
+    def draw_ellipse(self, img, center, axes, angle, startAngle, endAngle):
         thickness = 14
         lineType = cv2.LINE_AA
         shift = 10
+        color = (255, 255, 255)
         # uses the shift to accurately get sub-pixel resolution for arc
         center = (
             int(round(center[0] * 2**shift)),
@@ -134,41 +133,37 @@ class Intersection:
             int(round(axes[0] * 2**shift)),
             int(round(axes[1] * 2**shift))
         )
-        color = (255,255,255)
 
         cv2.ellipse(img, center, axes, angle,
                     startAngle, endAngle, color,
                     thickness, lineType, shift)
 
-    def cornerHarris(self,img):
-
-        corners = cv2.goodFeaturesToTrack(img,25,0.01,10)
+    def cornerHarris(self, img):
+        corners = cv2.goodFeaturesToTrack(img, 25, 0.01, 10)
 
         if corners is not None:
             corners = np.int0(corners)
             for i in corners:
-                x,y = i.ravel()
-                cv2.circle(img,(x,y),3,255,-1)
+                x, y = i.ravel()
+                cv2.circle(img, (x, y), 3, 255, -1)
                 
                 self.x_points.append(x)
                 self.y_points.append(y)
 
-
     def small_right_turn(self, yaw_init):
         
         lane_keeping_threshold = 22
-        increasing_angle_threshold = [90-10, 90+10]
+        increasing_angle_threshold = [80, 100]
         while self.finished is False:
             
             absolute_yaw_diff = np.abs(np.abs(yaw_init) - np.abs(self.get_perc()['Yaw']))
+            self.cam_frame = self.get_perc()['Camera']
 
-            self.cam_frame=self.get_perc()['Camera']
-
-            if  (absolute_yaw_diff > lane_keeping_threshold) or (self.reached is True ):
-                if (increasing_angle_threshold[0] < absolute_yaw_diff < increasing_angle_threshold[1]):
+            if (absolute_yaw_diff > lane_keeping_threshold) or (self.reached is True):
+                if increasing_angle_threshold[0] < absolute_yaw_diff < increasing_angle_threshold[1]:
                     self.finished = True
                     self.increase_angle = False
-                else :
+                else:
                     print('====== GRADUALLY INCREASING ANGLE ======')
                     self.increase_angle = True
                     self.reached = True
@@ -177,37 +172,35 @@ class Intersection:
                 cannyimg = self.canny(self.cam_frame)
                 maskedimg = self.mask(cannyimg, case="SmallRight")
                 
-                point1 = (int(self.width/3),int(self.height))
-                a,b = self.thresh_callback(maskedimg)
-                point2 = (a,b)
+                point1 = (int(self.width/3), int(self.height))
+                a, b = self.thresh_callback(maskedimg)
+                point2 = (a, b)
                 sagitta = 40
                 center, radius, start_angle, end_angle = self.convert_arc(point1, point2, sagitta)
                 
                 axes = (radius, radius)
-                self.lane_frame_int = np.zeros((self.height, self.width, 3), dtype = np.uint8)  
-                self.draw_ellipse(self.lane_frame_int, center, axes, 0, start_angle, end_angle, 255)
+                self.lane_frame_int = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+                self.draw_ellipse(self.lane_frame_int, center, axes, 0, start_angle, end_angle)
              
             time.sleep(0.1)
 
     def straight_yaw(self, yaw_init):
-        startD = time.time()
+        start = time.time()
         self.yaw_angle = True
 
         while self.finished is False:
             speed = self.get_perc()['Speed']
             self.yaw_diff = np.abs(yaw_init) - np.abs(self.get_perc()['Yaw'])
-            endD = time.time()
+            end = time.time()
             endFlag = 140/speed
             
-            if endD-startD > endFlag:
+            if end-start > endFlag:
                 self.yaw_angle = False
                 self.finished = True
-
 
     def big_left_turn(self, yaw_init, init_diff):
         lane_keeping_threshold = 18
         decreasing_angle_threshold = [80, 90]
-
         correction = True
         self.yaw_diff = np.abs(yaw_init) - np.abs(self.get_perc()['Yaw'])
 
@@ -215,11 +208,11 @@ class Intersection:
 
             absolute_yaw_diff = np.abs(np.abs(yaw_init) - np.abs(self.get_perc()['Yaw']))
 
-            if (absolute_yaw_diff > lane_keeping_threshold) or (self.reached is True ):
-                if (decreasing_angle_threshold[0] < absolute_yaw_diff  < decreasing_angle_threshold[1]):
+            if (absolute_yaw_diff > lane_keeping_threshold) or (self.reached is True):
+                if decreasing_angle_threshold[0] < absolute_yaw_diff < decreasing_angle_threshold[1]:
                     self.finished = True
                     self.decrease_angle = False
-                else :
+                else:
                     print('====== GRADUALLY DECREASING ANGLE ======')
                     self.angle_step = -abs(self.angle_step)
                     self.decrease_angle = True
@@ -227,7 +220,7 @@ class Intersection:
 
             if not self.reached:      
                 if abs(absolute_yaw_diff) > 7.0 and correction is True:
-                    # print("STRANGE MODEEE")
+                    # print("STRANGE MODE")
                     if init_diff > 0.0:
                         self.angle_step = -abs(self.angle_step)
                     else:
@@ -239,22 +232,22 @@ class Intersection:
                     # print("STARTING LANE")
                     
                     self.decrease_angle = False
-                    self.cam_frame=self.get_perc()['Camera']
+                    self.cam_frame = self.get_perc()['Camera']
                     cannyimg = self.canny(self.cam_frame)
                     maskedimg = self.mask(cannyimg, case="BigLeft")
                     self.cornerHarris(maskedimg)
 
-                    point1 = (int(self.width),int(self.height*0.9))                
+                    point1 = (int(self.width), int(self.height*0.9))
                     a = min(self.x_points) 
                     index = self.x_points.index(a)
                     b = self.y_points[index]
-                    point2 = (a+70,b)
+                    point2 = (a+70, b)
                     sagitta = -30
                     center, radius, start_angle, end_angle = self.convert_arc(point1, point2, sagitta)
                     axes = (radius, radius)
 
-                    self.lane_frame_int = np.zeros((self.height, self.width, 3), dtype = np.uint8)  
-                    self.draw_ellipse(self.lane_frame_int, center, axes, 0, start_angle, end_angle, 255)
+                    self.lane_frame_int = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+                    self.draw_ellipse(self.lane_frame_int, center, axes, 0, start_angle, end_angle)
                     # self.draw_ellipse(self.img, center, axes, 0, start_angle, end_angle, 255)
                     
                     self.x_points = []
